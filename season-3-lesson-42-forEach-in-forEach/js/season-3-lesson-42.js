d0aa9cdfb3edb03d754dfcdf02a6516547298ded
Alty <heneveraskedforthis@gmail.com>
2023-03-15 02:49:45 +0300

    season 3 lesson forEach in forEach

---
+++ season-3-lesson-42-forEach-in-forEach/js/season-3-lesson-42.js
@@ -0,0 +1,15 @@
+let array = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+];
+
+array.forEach(function (elem) {
+    console.log('outer loop ' + elem);
+
+    elem.forEach(function (elem) {
+        console.log('inner loop ' + elem);
+    });
+});
+
+/*-------------------------------------------------------------------------------------------------------------*/
