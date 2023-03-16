f2d2ece4bda682f56d8f4616b5d293f6ff81e106
Alty <heneveraskedforthis@gmail.com>
2023-03-16 11:27:38 +0300

    season 3 lesson 49 Canvas part 06: Marching ants

---
+++ season-3-lesson-49-Canvas-part-06-Marching-ants/js/season-3-lesson-49.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementsByClassName('canvas-one')[0];
+const ctx = canvas.getContext('2d');
+
+/*-------------------------------------------------------------------------------------------------------------*/
+
+let offset = 0;
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.setLineDash([4, 2]);
+    ctx.lineDashOffset = -offset;
+    ctx.strokeRect(10, 10, 100, 100);
+};
+
+const march = () => {
+    offset++;
+
+    if (offset > 16) {
+        offset = 0;
+    };
+
+    draw();
+
+    setTimeout(march, 20);
+};
+
+march();
