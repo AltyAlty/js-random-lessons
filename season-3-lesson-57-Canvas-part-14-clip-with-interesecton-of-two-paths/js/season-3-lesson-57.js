e7c44461dd82af1ef39178641f72ade6a25daf92
Alty <heneveraskedforthis@gmail.com>
2023-03-23 05:59:10 +0300

    season 3 lesson 57 Canvas part 14: clip with interesecton of two paths

---
+++ season-3-lesson-57-Canvas-part-14-clip-with-interesecton-of-two-paths/js/season-3-lesson-57.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementsByClassName('canvas-one')[0];
+const ctx = canvas.getContext('2d');
+
+/*-------------------------------------------------------------------------------------------------------------*/
+
+// Создаем два пути
+let circlePath = new Path2D();
+circlePath.arc(150, 75, 75, 0, 2 * Math.PI);
+let squarePath = new Path2D();
+squarePath.rect(85, 10, 130, 130);
+
+// Превращаем круг в область отсечения
+ctx.clip(circlePath);
+// Делаем так, чтобы областью отсечения стало пересечение первого и второго путей.
+ctx.clip(squarePath);
+
+// Draw stuff that gets clipped
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
