f42578f0fe260fc0fb47aaba3fa8fcb74d2ee9a5
Alty <heneveraskedforthis@gmail.com>
2023-04-01 12:43:57 +0300

    season 3 lesson 68 Canvas part 25: imageSmoothingEnabled, imageSmoothingQuality

---
+++ season-3-lesson-68-Canvas-part-25-imageSmoothingEnabled-imageSmoothingQuality/js/season-3-lesson-68.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementsByClassName('canvas-one')[0];
+const ctx = canvas.getContext('2d');
+
+/*-------------------------------------------------------------------------------------------------------------*/
+
+const img = new Image();
+img.src = './src/src.jpg';
+
+img.onload = () => {
+    const w = img.width;
+    const h = img.height;
+
+    ctx.drawImage(img, 0, 24, w, h);
+
+    /*Свойство "imageSmoothingEnabled" определяет, сглаживаются ли масштабированные изображения или нет.*/
+    ctx.imageSmoothingEnabled = true;
+    /*Свойство "imageSmoothingQuality" устанавливает качество сглаживания изображения. Может принимать значения
+    "low", "medium" и "high".*/
+    ctx.imageSmoothingQuality = 'low';
+    ctx.drawImage(img, w, 24, w * 1.5, h * 1.5);
+
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(img, w * 2.5, 24, w * 1.5, h * 1.5);
+};
