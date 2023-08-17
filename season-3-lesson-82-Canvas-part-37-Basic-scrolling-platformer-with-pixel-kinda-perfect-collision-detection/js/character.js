f15783bc949655c5f83bad2eca0fe7f99453ea05
Alty <heneveraskedforthis@gmail.com>
2023-08-17 20:43:25 +0300

    season 3 lesson 82 Canvas part 37 Basic scrolling platformer with pixel kinda perfect collision detection

---
+++ season-3-lesson-82-Canvas-part-37-Basic-scrolling-platformer-with-pixel-kinda-perfect-collision-detection/js/character.js
@@ -0,0 +1,331 @@
+function Character(x, y, width, height, maxJumpHeight, runningSpriteRight, runningSpriteLeft) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.currentSpeedX = 0;
+    this.currentDirectionXForDrawing = 'right';
+    this.currentDirectionX = 'right';
+    this.currentDirectionY = 'down';
+    this.downwardForce = 0;
+    this.currentJumpHeight = 0;
+    this.maxJumpHeight = maxJumpHeight;
+    this.runningSpriteRight = runningSpriteRight;
+    this.runningSpriteLeft = runningSpriteLeft;
+    this.leadingEdgeX = 0;
+    this.trailingEdgeX = 0;
+
+    this.prepareCharacterData = function () {
+        // this.defineDirectionX();
+        // this.defineDirectionY();
+        this.defineDirectionXForDrawing();
+        this.applyGravity();
+        this.applyMovementX();
+        this.applyMovementY();
+    };
+
+    this.applyGravity = function () {
+        if (this.findIfCharacterIsJumping()) {
+            this.currentJumpHeight += this.downwardForce * -1;
+
+            if (this.currentJumpHeight >= this.maxJumpHeight) {
+                this.downwardForce = world.gravity;
+                this.currentJumpHeight = 0;
+            };
+        } else {
+            if (!this.findIfPlayerIsStandingOnAPlatform()) { this.downwardForce = world.gravity };
+        };
+    };
+
+    this.applyMovementX = function () {
+        let nextX = this.x + this.currentSpeedX;
+
+        if (this.currentSpeedX !== 0) {
+
+            let predictedHorizontalPosition = {
+                x: nextX,
+                y: this.y,
+                width: this.width,
+                height: this.height
+            };
+
+            for (let i = 0; i < world.worldGrid.length; i++) {
+                for (let j = 0; j < world.worldGrid[i].length; j++) {
+                    if (
+                        helper.checkIntersectionBetweenTwoNotRotatedRectangles(
+                            predictedHorizontalPosition.x + predictedHorizontalPosition.width, world.worldGrid[i][j][0],
+                            predictedHorizontalPosition.x, world.worldGrid[i][j][0] + world.worldGrid[i][j][2],
+                            predictedHorizontalPosition.y + predictedHorizontalPosition.height, world.worldGrid[i][j][1],
+                            predictedHorizontalPosition.y, world.worldGrid[i][j][1] + world.worldGrid[i][j][3]
+                        ) && world.worldGrid[i][j][4] === true
+                    ) {
+
+                        if (this.checkPixelCollisionLeftRightDownUp(predictedHorizontalPosition.x, predictedHorizontalPosition.y,
+                            predictedHorizontalPosition.width, predictedHorizontalPosition.height) && this.currentSpeedX > 0) { // ⇒
+
+                            console.log('Checking RIGHT');
+
+                            while (this.checkPixelCollisionLeftRightDownUp(predictedHorizontalPosition.x, predictedHorizontalPosition.y,
+                                predictedHorizontalPosition.width, predictedHorizontalPosition.height)
+                            ) {
+                                predictedHorizontalPosition.x -= Math.sign(this.currentSpeedX);
+                            };
+
+                            nextX = predictedHorizontalPosition.x;
+                            this.currentSpeedX = 0;
+                        };
+
+                        if (this.checkPixelCollisionRightLeftDownUp(predictedHorizontalPosition.x, predictedHorizontalPosition.y,
+                            predictedHorizontalPosition.width, predictedHorizontalPosition.height) && this.currentSpeedX < 0) { // ⇐
+
+                            console.log('Checking LEFT');
+
+                            while (this.checkPixelCollisionRightLeftDownUp(predictedHorizontalPosition.x, predictedHorizontalPosition.y,
+                                predictedHorizontalPosition.width, predictedHorizontalPosition.height)
+                            ) {
+                                predictedHorizontalPosition.x -= Math.sign(this.currentSpeedX);
+                            };
+
+                            nextX = predictedHorizontalPosition.x;
+                            this.currentSpeedX = 0;
+                        };
+
+                        this.x = nextX;
+                        return;
+                    };
+                };
+            };
+        };
+
+        this.x = nextX;
+    };
+
+    this.applyMovementY = function () {
+        let nextY = this.y + this.downwardForce;
+
+        if (this.downwardForce !== 0) {
+
+            let predictedVerticalPosition = {
+                x: this.x,
+                y: nextY,
+                width: this.width,
+                height: this.height
+            };
+
+            for (let i = 0; i < world.worldGrid.length; i++) {
+                for (let j = 0; j < world.worldGrid[i].length; j++) {
+                    if (
+                        helper.checkIntersectionBetweenTwoNotRotatedRectangles(
+                            predictedVerticalPosition.x + predictedVerticalPosition.width, world.worldGrid[i][j][0],
+                            predictedVerticalPosition.x, world.worldGrid[i][j][0] + world.worldGrid[i][j][2],
+                            predictedVerticalPosition.y + predictedVerticalPosition.height, world.worldGrid[i][j][1],
+                            predictedVerticalPosition.y, world.worldGrid[i][j][1] + world.worldGrid[i][j][3]
+                        ) && world.worldGrid[i][j][4] === true
+                    ) {
+
+                        if (this.checkPixelCollisionLeftRightDownUp(predictedVerticalPosition.x, predictedVerticalPosition.y,
+                            predictedVerticalPosition.width, predictedVerticalPosition.height) && this.downwardForce < 0) { // ⇑
+
+                            console.log('Checking UP');
+
+                            while (this.checkPixelCollisionLeftRightDownUp(predictedVerticalPosition.x, predictedVerticalPosition.y,
+                                predictedVerticalPosition.width, predictedVerticalPosition.height)
+                            ) {
+                                predictedVerticalPosition.y -= Math.sign(this.downwardForce);
+                            };
+
+                            nextY = predictedVerticalPosition.y;
+                            this.downwardForce = 0;
+                            this.currentJumpHeight = 0;
+                        };
+
+                        if (this.checkPixelCollisionLeftRightUpDown(predictedVerticalPosition.x, predictedVerticalPosition.y,
+                            predictedVerticalPosition.width, predictedVerticalPosition.height) && this.downwardForce > 0) { // ⇓
+
+                            console.log('Checking DOWN');
+
+                            while (this.checkPixelCollisionLeftRightUpDown(predictedVerticalPosition.x, predictedVerticalPosition.y,
+                                predictedVerticalPosition.width, predictedVerticalPosition.height)
+                            ) {
+                                predictedVerticalPosition.y -= Math.sign(this.downwardForce);
+                            };
+
+                            nextY = predictedVerticalPosition.y;
+                            this.downwardForce = 0;
+                            this.currentJumpHeight = 0;
+                        };
+
+                        this.y = nextY;
+                        return;
+                    };
+                };
+            };
+        };
+
+        this.y = nextY;
+    };
+
+    this.findIfCharacterIsJumping = function () { return this.downwardForce < 0 };
+    this.isFalling = function () { return this.downwardForce > 0 };
+    this.findIfCharacterIsMovingX = function () { return this.currentSpeedX !== 0 };
+
+    this.findLeadingEdgeXOfCharacter = function () {
+        if (this.currentSpeedX < 0) { return this.leadingEdgeX = this.x }
+        else { return this.leadingEdgeX = this.x + this.width };
+    };
+
+    this.findIfPlayerIsStandingOnAPlatform = function () {
+        for (let i = this.x; i <= this.x + width; i++) {
+            if (world.findIfPixelIsSolidSurface(i, this.y + this.height + 1)) { return true };
+        };
+
+        return false;
+    };
+
+    this.defineDirectionX = function () {
+        if (this.currentSpeedX > 0) { this.currentDirectionX = 'right' }
+        else if (this.currentSpeedX < 0) { this.currentDirectionX = 'left' }
+        else { this.currentDirectionX = 'noXdirection' };
+    };
+
+    this.defineDirectionY = function () {
+        if (this.downwardForce > 0) { this.currentDirectionY = 'down' }
+        else if (this.downwardForce < 0) { this.currentDirectionY = 'up' }
+        else { this.currentDirectionY = 'noYdirection' };
+    };
+
+    this.drawHitbox = function (drawAtX) {
+        ctx.strokeStyle = 'rgb(234, 0, 255)';
+        ctx.lineWidth = 1;
+        ctx.strokeRect(drawAtX, this.y, this.width, this.height);
+    };
+
+    this.defineDirectionXForDrawing = function () {
+        if (this.currentSpeedX > 0) { this.currentDirectionXForDrawing = 'right' }
+        else if (this.currentSpeedX < 0) { this.currentDirectionXForDrawing = 'left' };
+    };
+
+    this.draw = function () {
+        let drawAtX = this.x - world.distanceTravelledFromSpawnPoint;
+
+        /*Проверяем не прошел ли игрок дальше точки спавна в левую сторону, и если так, то двигаем изображения персонажа.*/
+        drawAtX = drawAtX > this.x ? this.x : drawAtX;
+
+        /*Проверяем не находится ли игрок в конце уровня в правой стороне, и если так, то двигаем изображения персонажа.*/
+        if (world.findIfPlayerIsAtLevelEnd()) {
+            drawAtX = world.screenWidth - (world.levelImage.width - world.distanceTravelledFromSpawnPoint - (this.x - world.distanceTravelledFromSpawnPoint));
+        };
+
+        let sprite = null;
+
+        if (this.currentDirectionXForDrawing === 'right') { sprite = this.runningSpriteRight }
+        else if (this.currentDirectionXForDrawing === 'left') { sprite = this.runningSpriteLeft };
+
+        if (!game.finished) {
+            this.drawHitbox(drawAtX);
+
+            if (this.findIfCharacterIsJumping() || this.isFalling()) {
+                sprite.drawFrame(4, drawAtX, this.y, this.width, this.height);
+            } else if (this.findIfCharacterIsMovingX()) {
+                sprite.drawAnimation(game.ticks, drawAtX, this.y, this.width, this.height);
+            } else {
+                sprite.drawFrame(1, drawAtX, this.y, this.width, this.height);
+            };
+        };
+    };
+
+    this.collidesWith = function (other) {
+        if (this.x >= other.x &&
+            this.x <= other.x + other.width &&
+            this.y >= other.y &&
+            this.y <= other.y + other.height) {
+            return true;
+        };
+
+        return false;
+    };
+
+    this.checkPixelCollisionUpDownLeftRight = function (x, y, width, height) {
+        for (let i = x; i <= x + width; i++) {
+            for (let j = y; j <= y + height; j++) {
+                if (world.findIfPixelIsSolidSurface(i, j)) {
+                    return { x: i, y: j };
+                };
+            };
+        };
+
+        return null;
+    };
+
+    this.checkPixelCollisionUpDownRightLeft = function (x, y, width, height) {
+        for (let i = x + width; i >= x; i--) {
+            for (let j = y; j <= y + height; j++) {
+                if (world.findIfPixelIsSolidSurface(i, j)) {
+                    return { x: i, y: j };
+                };
+            };
+        };
+
+        return null;
+    };
+
+    this.checkPixelCollisionDownUpLeftRight = function (x, y, width, height) {
+        for (let i = x; i <= x + width; i++) {
+            for (let j = y + height; j >= y; j--) {
+                if (world.findIfPixelIsSolidSurface(i, j)) {
+                    return { x: i, y: j };
+                };
+            };
+        };
+
+        return null;
+    };
+
+    this.checkPixelCollisionDownUpRightLeft = function (x, y, width, height) {
+        for (let i = x + width; i >= x; i--) {
+            for (let j = y + height; j >= y; j--) {
+                if (world.findIfPixelIsSolidSurface(i, j)) {
+                    return { x: i, y: j };
+                };
+            };
+        };
+
+        return null;
+    };
+
+    this.checkPixelCollisionLeftRightUpDown = function (x, y, width, height) {
+        for (let i = y; i <= y + height; i++) {
+            for (let j = x; j <= x + width; j++) {
+                if (world.findIfPixelIsSolidSurface(j, i)) {
+                    return { x: j, y: i };
+                };
+            };
+        };
+
+        return null;
+    };
+
+    this.checkPixelCollisionLeftRightDownUp = function (x, y, width, height) {
+        for (let i = y + height; i >= y; i--) {
+            for (let j = x; j <= x + width; j++) {
+                if (world.findIfPixelIsSolidSurface(j, i)) {
+                    return { x: j, y: i };
+                };
+            };
+        };
+
+        return null;
+    };
+
+    this.checkPixelCollisionRightLeftDownUp = function (x, y, width, height) {
+        for (let i = y + height; i >= y; i--) {
+            for (let j = x + width; j >= x; j--) {
+                if (world.findIfPixelIsSolidSurface(j, i)) {
+                    return { x: j, y: i };
+                };
+            };
+        };
+
+        return null;
+    }
+};
---
+++ season-3-lesson-82-Canvas-part-37-Basic-scrolling-platformer-with-pixel-kinda-perfect-collision-detection/js/world.js
@@ -0,0 +1,126 @@
+const world = {
+    screenWidth: canvas.width,
+    screenHeight: canvas.height,
+    gravity: 10,
+    distanceTravelledFromSpawnPoint: 0,
+    levelImage: null,
+    collisionMapImage: null,
+    collisionMapCanvas2DContext: null,
+    collisionMapWidth: 8000,
+    collisionMapHeight: 480,
+    worldGrid: [],
+    worldGridCellSize: 80,
+
+    loadLevelImage: function () {
+        this.levelImage = new Image();
+        this.levelImage.src = './src/level/map-three.png';
+    },
+
+    loadCollisionMapCanvas2DContext: function () {
+        this.collisionMapImage = new Image();
+        this.collisionMapImage.src = collisionMapImageBase64;
+        this.collisionMapImage.onload = function () {
+            let hiddenCanvas = document.createElement('canvas');
+            hiddenCanvas.setAttribute('width', this.width); // 8000
+            hiddenCanvas.setAttribute('height', this.height); // 480
+            world.collisionMapCanvas2DContext = hiddenCanvas.getContext('2d', { willReadFrequently: true });
+            world.collisionMapCanvas2DContext.drawImage(this, 0, 0);
+        };
+    },
+
+    fillWorldGrid: function () {
+        let rows = this.collisionMapHeight / this.worldGridCellSize;
+        let columns = this.collisionMapWidth / this.worldGridCellSize;
+
+        for (let i = 0; i < rows; i++) { // y
+            this.worldGrid.push([]);
+
+            for (let j = 0; j < columns; j++) { // x
+
+                if (helper.checkPixelCollisionUpDownLeftRight(j * this.worldGridCellSize, i * this.worldGridCellSize, this.worldGridCellSize, this.worldGridCellSize)) {
+
+                    this.worldGrid[i].push(
+                        [
+                            j * this.worldGridCellSize,
+                            i * this.worldGridCellSize,
+                            this.worldGridCellSize,
+                            this.worldGridCellSize,
+                            true
+                        ]
+                    );
+
+                } else {
+
+                    this.worldGrid[i].push(
+                        [
+                            j * this.worldGridCellSize,
+                            i * this.worldGridCellSize,
+                            this.worldGridCellSize,
+                            this.worldGridCellSize,
+                            false
+                        ]
+                    );
+
+                };
+            };
+        };
+    },
+
+    findIfPixelIsSolidSurface: function (x, y) {
+        return this.getPixelType(x, y) === '#';
+    },
+
+    getPixelType: function (x, y) {
+        if (!this.collisionMapCanvas2DContext) return '.';
+
+        let rawPixelData = this.collisionMapCanvas2DContext.getImageData(x, y, 1, 1).data; // []
+        let RGBAPixelData = `${rawPixelData[0]} ${rawPixelData[1]} ${rawPixelData[2]} ${rawPixelData[3]}`;
+
+        if (RGBAPixelData === '255 0 0 255') return 'pit';
+        if (RGBAPixelData === '76 255 0 255') return 'exit';
+        if (RGBAPixelData === '255 255 255 255') return '.';
+        if (RGBAPixelData === '0 0 0 255') return '#';
+    },
+
+    prepareWorldData: function () {
+        if (!this.levelImage) { this.loadLevelImage() };
+        if (!this.collisionMapCanvas2DContext) { this.loadCollisionMapCanvas2DContext() };
+        this.distanceTravelledFromSpawnPoint += player.character.currentSpeedX;
+    },
+
+    findXOfBeginningOfLevelEnd: function () {
+        return this.levelImage.width - this.screenWidth;
+    },
+
+    findIfPlayerIsAtLevelEnd: function () {
+        return this.distanceTravelledFromSpawnPoint >= this.findXOfBeginningOfLevelEnd();
+    },
+
+    drawWordlGrid: function (drawAtX) {
+        let rows = this.collisionMapHeight / this.worldGridCellSize;
+        let columns = this.collisionMapWidth / this.worldGridCellSize;
+
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = 'orange';
+
+        for (let i = 0; i < rows; i++) { // y
+            for (let j = 0; j < columns; j++) { // x
+                ctx.strokeRect(j * this.worldGridCellSize + drawAtX, i * this.worldGridCellSize, this.worldGridCellSize, this.worldGridCellSize);
+            };
+        };
+    },
+
+    draw: function () {
+        /*Умножаем на -1, так как при движении игрока в какую-то сторону, мы должны двигать изображение уровня в противоположную сторону.*/
+        let drawAtX = this.distanceTravelledFromSpawnPoint * -1;
+        /*Проверяем не прошел ли игрок дальше точки спавна в левую сторону, и если так, то перестаем двигать изображения уровня.*/
+        drawAtX = drawAtX > 0 ? 0 : drawAtX;
+        /*Проверяем не находится ли игрок в конце уровня в правой стороне, и если так, то перестаем двигать изображение уровня.*/
+        drawAtX = this.findIfPlayerIsAtLevelEnd() ? this.findXOfBeginningOfLevelEnd() * -1 : drawAtX;
+
+        if (!game.finished) { ctx.drawImage(this.levelImage, drawAtX, 0) };
+
+        this.drawWordlGrid(drawAtX);
+    }
+};
+
