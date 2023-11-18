2c5f193539ee0b2442aa2cb17184960893b598ed
Alty <heneveraskedforthis@gmail.com>
2023-11-18 17:22:19 +0300

    season 3 lesson 84 add more info

--- season-3-lesson-84-Forms/js/season-3-lesson-84.js
+++ season-3-lesson-84-Forms/js/season-3-lesson-84.js
@@ -1,1 +1,15 @@
+document.addEventListener('blur', function (event) {
+    // Validate the field
+    const isValid = event.target.validity.valid;
+    const message = event.target.validationMessage;
+    const connectedValidationId = event.target.getAttribute('aria-describedby');
+    const connectedValidation = connectedValidationId ? document.getElementById(connectedValidationId) : false;
+
+    if (connectedValidation && message && !isValid) {
+        connectedValidation.innerText = message;
+    } else {
+        connectedValidation.innerText = '';
+    }
+}, true);
+
 /*--------------------------------------------------------------------------------------------------------------------------------------------*/
