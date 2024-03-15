e29ea8e858248d1a13d6a8e4712d7e2ce659a6e5
Alty <heneveraskedforthis@gmail.com>
2024-03-15 20:01:34 +0300

    season 4 lessons 024 add more info

--- season-4-lesson-024-Arrays-in-JS-part-004-a-lot-of-methods/js/season-4-lesson-024.js
+++ season-4-lesson-024-Arrays-in-JS-part-004-a-lot-of-methods/js/season-4-lesson-024.js
@@ -34,7 +34,7 @@ console.log(array2); // [1, 6, 8, 9, 7, 10, 11]
 console.log(array2.splice(2)); // [8, 9, 7, 10, 11]
 console.log(array2); // [1, 6]
 
-let obj0 = {a: 1};
+let obj0 = { a: 1 };
 let array2_1 = [1, 2, obj0];
 let item0 = array2_1.splice(2, 1);
 console.log(item0);
@@ -66,7 +66,7 @@ let array5 = [9, 10]
 console.log(array4.concat([6, 7])); // [1, 2, 3, 4, 5, 6, 7]
 console.log(array4.concat([6, 7], 8)); // [1, 2, 3, 4, 5, 6, 7, 8]
 console.log(array4.concat([6, 7], 8, array5)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(array4.concat([6, 7], [], {a: 1})); // [1, 2, 3, 4, 5, 6, 7, { a: 1 }]
+console.log(array4.concat([6, 7], [], { a: 1 })); // [1, 2, 3, 4, 5, 6, 7, { a: 1 }]
 
 /*Если использовать массивоподобный объект со специальным свойством "Symbol.isConcatSpreadable", то он обработается
 как массив.*/
@@ -142,13 +142,13 @@ console.log('--------------------------------------');
 /*Метод "find()" перебирает массив и ищет элемент в соотвествии с условием в виде указанной функции. Этот метод берет
 каждый элемент массива и вызывает с ним в качестве параметра указанную функцию. Если указанная функция возвращает true, 
 то поиск прекращается и возвращается найденный элемент, иначе возвращается undefined. Указаннная функция может 
-принимать до трех параметров: элемент, индекс элемента и сам массив. */
+принимать до трех параметров: элемент, индекс элемента и сам массив.*/
 let array9 = [
-    {a: 1},
-    {a: 2},
-    {a: 3},
-    {a: 3},
-    {a: 5}
+    { a: 1 },
+    { a: 2 },
+    { a: 3 },
+    { a: 3 },
+    { a: 5 }
 ];
 
 let item1 = array9.find(
@@ -179,11 +179,11 @@ console.log('--------------------------------------');
 
 /*Метод "filter()" работает также как и метод "find()", только возвращает массив всех найденных элементов.*/
 let array10 = [
-    {a: 1},
-    {a: 2},
-    {a: 3},
-    {a: 3},
-    {a: 5}
+    { a: 1 },
+    { a: 2 },
+    { a: 3 },
+    { a: 3 },
+    { a: 5 }
 ];
 
 let items1 = array10.filter(
@@ -201,11 +201,11 @@ console.log('--------------------------------------');
 /*Метод "map()" вызывает указанную функцию для каждого элемента массива и формирует новый массив с результатами
 вызванных функций.*/
 let array11 = [
-    {a: 1},
-    {a: 2},
-    {a: 3},
-    {a: 3},
-    {a: 5}
+    { a: 1 },
+    { a: 2 },
+    { a: 3 },
+    { a: 3 },
+    { a: 5 }
 ];
 
 // let items2 = array11.map(
@@ -341,16 +341,286 @@ let obj2 = {
 };
 
 let array20 = [
-    {age: 14},
-    {age: 18},
-    {age: 20},
-    {age: 64},
-    {age: 65},
-    {age: 66},
-    {age: 85}
+    { age: 14 },
+    { age: 18 },
+    { age: 20 },
+    { age: 64 },
+    { age: 65 },
+    { age: 66 },
+    { age: 85 }
 ];
 
 let result4 = array20.filter(obj2.canJoin, obj1);
 
 console.log(result4.length); // 3
 console.log(result4);
+
+console.log('--------------------------------------');
+
+// #1 NORMAL
+// function camelize(str) {
+//     return str
+//         .split('-')
+//         .map(
+//             (item, index) => {
+//                 if (index !== 0) {
+//                     return item[0].toUpperCase() + item.slice(1);
+//                 } else {
+//                     return item;
+//                 };
+//             }
+//         ).join('');
+// };
+
+// console.log(camelize('background-color'));
+// console.log(camelize('-webkit-transition'));
+// console.log('--------------------------------------');
+
+// #2 EASY
+// let arr = [5, 3, 8, 1];
+
+// function filterRange(arr, a, b) {
+//     return arr.filter(
+//         (item) => {
+//             if (item >= a && item <= b) {
+//                 return true
+//             };
+//         }
+//     );
+// };
+
+// let filtered = filterRange(arr, 1, 4);
+// console.log(filtered);
+// console.log(arr);
+// console.log('--------------------------------------');
+
+// #3 HARD Сортировка массива на месте.
+// let arr = [5, 3, 2, 1, 8, 6, 7, 1];
+
+// function filterRangeInPlace(arr, a, b) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < a || arr[i] > b) {
+//             arr.splice(i, 1);
+//             i--;
+//         };
+//     };
+// };
+
+// filterRangeInPlace(arr, 1, 4);
+// console.log(arr);
+// console.log('--------------------------------------');
+
+// #4 EASY
+// let arr = [5, 2, 1, -10, 8];
+// arr.sort((a, b) => b - a);
+// console.log(arr);
+// console.log('--------------------------------------');
+
+// #5 EASY
+// let arr = ['HTML', 'JavaScript', 'CSS'];
+
+// function copySorted(arr) {
+//     arr.slice(0).sort();
+// };
+
+// let sorted = copySorted(arr);
+// console.log(sorted);
+// console.log(arr);
+// console.log('--------------------------------------');
+
+// #6 HARD Расширяемый калькулятор.
+// function Calculator() {
+//     this.methods = {
+//         '+': (a, b) => a + b,
+//         '-': (a, b) => a - b
+//     },
+
+//     this.calculate = function (str) {
+//         let participants = str.split(' ');
+//         let a = +participants[0];
+//         let operation = participants[1];
+//         let b = +participants[2];
+
+//         if (!this.methods[operation] || isNaN(a) || isNaN(b)) {
+//             return NaN;
+//         };
+
+//         console.log(this.methods[operation](a, b));
+//     },
+
+//     this.addMethod = function(str, func) {
+//         this.methods[str] = func;
+//     }
+// };
+
+// let powerCalc = new Calculator;
+// powerCalc.addMethod('*', (a, b) => a * b);
+// powerCalc.addMethod('/', (a, b) => a / b);
+// powerCalc.addMethod('**', (a, b) => a ** b);
+// powerCalc.calculate('2 ** 3');
+// console.log('--------------------------------------');
+
+// #7 EASY
+// let vasya = { name: 'Вася', age: 25 };
+// let petya = { name: 'Петя', age: 30 };
+// let masha = { name: 'Маша', age: 28 };
+// let users = [vasya, petya, masha];
+
+// let names = users.map(
+//     (item) => item.name
+// );
+
+// console.log(names);
+// console.log('--------------------------------------');
+
+// #8 EASY
+// let vasya = { name: 'Вася', surname: 'Пупкин', id: 1 };
+// let petya = { name: 'Петя', surname: 'Иванов', id: 2 };
+// let masha = { name: 'Маша', surname: 'Петрова', id: 3 };
+// let users = [vasya, petya, masha];
+
+// let usersMapped = users
+//     .slice(0)
+//     .map(
+//         (item) => {
+//             return {
+//                 fullname: item.name + ' ' + item.surname,
+//                 id: item.id
+//             }
+//         }
+//     );
+
+// console.log(usersMapped);
+// console.log(users);
+// console.log('--------------------------------------');
+
+// #9 EASY
+// let vasya = { name: 'Вася', age: 25 };
+// let petya = { name: 'Петя', age: 30 };
+// let masha = { name: 'Маша', age: 28 };
+// let arr = [vasya, petya, masha];
+
+// function sortByAge(arr) {
+//     arr.sort((a, b) => a.age - b.age);
+// };
+
+// sortByAge(arr);
+// console.log(arr);
+// console.log('--------------------------------------');
+
+// #10 VERY HARD Перемешивание массива
+// // Вариант №1
+// let arr = [1, 2, 3];
+
+// function shuffle(array) {
+//     array.sort(() => Math.random() - 0.5);
+// };
+
+// // подсчет вероятности для всех возможных вариантов
+// let count = {
+//     '123': 0,
+//     '132': 0,
+//     '213': 0,
+//     '231': 0,
+//     '321': 0,
+//     '312': 0
+// };
+
+// for (let i = 0; i < 1000000; i++) {
+//     let array = [1, 2, 3];
+//     shuffle(array);
+//     count[array.join('')]++;
+// };
+
+// // показать количество всех возможных вариантов
+// console.log(count);
+
+// /*Вариант №2, используя алгоритм под названием Тасование Фишера - Йетса. Суть заключается в том, чтобы проходить по 
+// массиву в обратном порядке и менять местами каждый элемент со случайным элементом, который находится перед ним.*/
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+
+//         // поменять элементы местами
+//         // мы используем для этого синтаксис "деструктурирующее присваивание"
+//         // то же самое можно записать как:
+//         // let t = array[i]; array[i] = array[j]; array[j] = t
+//         [array[i], array[j]] = [array[j], array[i]];
+//     };
+// };
+
+// // подсчет вероятности для всех возможных вариантов
+// let count = {
+//     '123': 0,
+//     '132': 0,
+//     '213': 0,
+//     '231': 0,
+//     '321': 0,
+//     '312': 0
+// };
+
+// for (let i = 0; i < 1000000; i++) {
+//     let array = [1, 2, 3];
+//     shuffle(array);
+//     count[array.join('')]++;
+// };
+
+// // показать количество всех возможных вариантов
+// console.log(count);
+// console.log('--------------------------------------');
+
+// #11 EASY
+// let vasya = { name: 'Вася', age: 25 };
+// let petya = { name: 'Петя', age: 30 };
+// let masha = { name: 'Маша', age: 29 };
+// let arr = [vasya, petya, masha];
+// let getAverageAge = (arr) => arr.reduce((acc, item) => acc + item.age, 0) / arr.length;
+// console.log(getAverageAge(arr));
+// console.log('--------------------------------------');
+
+// #12 NORMAL
+// function unique(arr) {
+//     let finalArr = [];
+
+//     arr.forEach(
+//         (item) => {
+//             if (!finalArr.includes(item)) {
+//                 finalArr.push(item);
+//             };
+//         }
+//     );
+
+//     return finalArr;
+// };
+
+// let strings = ['кришна', 'кришна', 'харе', 'харе', 'харе', 'харе', 'кришна', 'кришна', ':-O'];
+// console.log(unique(strings));
+// console.log('--------------------------------------');
+
+// #13 NORMAL
+// let users = [
+//     { id: 'john', name: 'John Smith', age: 20 },
+//     { id: 'ann', name: 'Ann Smith', age: 24 },
+//     { id: 'pete', name: 'Pete Peterson', age: 31 },
+// ];
+
+// function groupById(arr) {
+//     let resultObj = {};
+
+//     for (let i = 0; i < arr.length; i++) {
+//         resultObj[arr[i].id] = arr[i];
+//     };
+
+//     return resultObj;
+
+//     // return arr.reduce(
+//     //     (obj, value) => {
+//     //         obj[value.id] = value;
+//     //         return obj;
+//     //     }, {});
+// };
+
+// let usersById = groupById(users);
+
+// console.log(usersById);
+// console.log('--------------------------------------');
