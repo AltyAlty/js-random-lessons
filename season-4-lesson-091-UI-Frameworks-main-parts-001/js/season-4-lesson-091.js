65090f11e0a05eb9a63dc0fe63ab522734a5c40e
Alty <heneveraskedforthis@gmail.com>
2025-01-30 16:59:12 +0300

    season 4 lesson 091 UI Frameworks: main parts 001

---
+++ season-4-lesson-091-UI-Frameworks-main-parts-001/js/season-4-lesson-091.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+/*Из чего состоят UI-фреймворки:
+
+1. Компоненты: <User prop="name" onClick="doSomething()" />
+2. Условный рендеринг
+3. Локальный стейт для отрисовки UI
+4. Получение данных с сервера (Promise -> Локальный стейт -> Перерисовка)
+5. One-way/Two-way binding
+6. Глобальный стейт/кэш: Redux/Veux/NgRx/react-query/class Services/Pinia
+7. Работа с формами
+8. Роутинг: 
+    1) URI query params, push/navigate
+    2) URL1 -> Component1; URL2 -> Component2
+    3) Guard/Auth/redirect
+9. i18n
+10. CSS
+11. Темы (Dark/Light)
+12. Observer Pattern
+13. Hooks, lifecycle methods
+14. ООП: Angular, MobX
+15. SOLID/GRASP/KISS/DRY
+16. M-V-C/M-V-P/M-V-VM*/
